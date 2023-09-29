dd2776b404d65a3c3b061b4b0c83de9ab7714602
James Carlson <jxxcarlson@gmail.com>
2023-09-29 00:51:09 -0400

    Remove Evergreen files

--- src/Evergreen/V102/Credentials.elm
+++
@@ -1,5 +0,0 @@
-module Evergreen.V102.Credentials exposing (..)
-
-
-type Credentials
-    = V1 String String
--- src/Evergreen/V104/FastDict.elm
+++
@@ -1,7 +0,0 @@
-module Evergreen.V104.FastDict exposing (..)
-
-import Evergreen.V104.Internal
-
-
-type alias Dict k v =
-    Evergreen.V104.Internal.InternalDict k v
