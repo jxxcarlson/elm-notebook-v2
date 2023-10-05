20d05f9dedd5bba57d81ac59c20829ddcafc23a6
James Carlson <jxxcarlson@gmail.com>
2023-10-05 03:04:19 -0400

    Rename endpoint

--- src/Notebook/Package.elm
+++ src/Notebook/Package.elm
@@ -9,7 +9,7 @@ import Types
 sendPackageList : Notebook.Types.PackageList -> Cmd Types.FrontendMsg
 sendPackageList packageList =
     Http.post
-        { url = "http://localhost:8009"
+        { url = "http://localhost:8009/packageList"
         , body = Http.jsonBody (Notebook.Codec.encodePackageList packageList)
         , expect = Http.expectWhatever Types.PackageListSent
         }
