d4c7529e7259449425b632ddd2b817a90c53a2e3
James Carlson <jxxcarlson@gmail.com>
2023-10-19 20:17:34 -0400

    Preparing for v82

---
+++ src/Evergreen/V82/Notebook/Cell.elm
@@ -0,0 +1,31 @@
+module Evergreen.V82.Notebook.Cell exposing (..)
+
+import Evergreen.V82.Notebook.Types
+
+
+type CellType
+    = CTCode
+    | CTMarkdown
+
+
+type CellValue
+    = CVString String
+    | CVMarkdown String
+    | CVNone
+
+
+type CellState
+    = CSEdit
+    | CSView
+
+
+type alias Cell =
+    { index : Int
+    , text : String
+    , tipe : CellType
+    , value : CellValue
+    , cellState : CellState
+    , locked : Bool
+    , report : Maybe (List Evergreen.V82.Notebook.Types.MessageItem)
+    , replData : Maybe Evergreen.V82.Notebook.Types.ReplData
+    }
