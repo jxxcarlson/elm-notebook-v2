fc41ce6b4590edd4fea294f5e60aeb78a8e5c2e9
James Carlson <jxxcarlson@gmail.com>
2023-10-28 17:37:55 -0400

    Use white for guest sign-in link

--- src/View/Header.elm
+++ src/View/Header.elm
@@ -55,7 +55,7 @@ notSignedInHeader model =
 welcomeLink =
     E.newTabLink []
         { url = "https://elm-notebook2.lamdera.app/open/jxxcarlson-welcome"
-        , label = E.el [ Font.underline, Font.color (E.rgb 0.65 0.65 1), Font.size 14 ] (E.text "Sign in as Guest")
+        , label = E.el [ Font.underline, Font.color (E.rgb 0.9 0.9 0.9), Font.size 14 ] (E.text "Sign in as Guest")
         }
 
 
