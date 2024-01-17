d61bdd314c4c6c0ae88ca6c7c9d89cdd8f71c7e1
James Carlson <jxxcarlson@gmail.com>
2024-01-17 01:20:00 -0500

    Fix broken wiring ports for copy text to clipboard

--- elm-pkg-js-includes.js
+++ elm-pkg-js-includes.js
@@ -1,10 +1,14 @@
 import * as evalJs from './elm-pkg-js/eval-js';
 import * as testPorts from './elm-pkg-js/test-ports';
 
+const copy_to_clipboard = require('./elm-pkg-js/copy-to-clipboard.js')
+
+
 exports.init = async function init(app) {
   // @WARNING: this only runs for Lamdera production deploys!
   // This file will not run in Local development, an equivalent to this is
   // automatically generated in Local Development for every file in elm-pkg-js/
+
 
   evalJs.init(app)
   copy_to_clipboard.init(app)
